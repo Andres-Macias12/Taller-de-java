f707afdec8f133494887d13866e8226126896a93
Andres Macias <andresmacias978@gmail.com>
2024-05-02 21:24:44 -0500

    Trabajo java

---
+++ src/test/java/TrabajosEnClase/Main2.java
@@ -0,0 +1,11 @@
+package TrabajosEnClase;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Coche coche = new Coche();
+        coche.acelerar(50);
+        coche.frenar();
+        coche.acelerar(90);
+        coche.frenar();
+    }
+}
